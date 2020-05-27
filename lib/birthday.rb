f9dcb152e6b983f87df4a57e7c77fd662079a361
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-05-27 23:32:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age_appropriate_birthday
+    if age_appropriate_birthday(birthday_kids)
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
       puts "You are too old for this."
